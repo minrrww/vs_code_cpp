2685d10b84d7d03ffa0354cff50382ed7264298b
minrrww <598169426@qq.com>
2022-06-12 00:37:03 +0800

    复习

--- primer/test.cpp
+++ primer/test.cpp
@@ -1,7 +1,12 @@
-#include <iostream> //#include 指令:告诉编译器，需使用头文件（header）iostream库
-#include <string>
+#include <iostream>
 #include <vector>
+using namespace std;
 int main()
 {
-
+    vector<int> ivec = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<int>::size_type cnt = ivec.size();
+    //将把从size到1的值赋给ivec的元素
+    for (vector<int>::size_type ix = 0; ix != ivec.size(); ++ix, --cnt)
+        cout << (ivec[ix] = cnt) << " ";
+    cout << endl;
 }
