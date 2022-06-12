4320cca5e8fa75f3e69bfc8c6836c5621ebcd0e1
minrrww <598169426@qq.com>
2022-06-12 15:18:01 +0800

    复习

--- primer/test.cpp
+++ primer/test.cpp
@@ -3,10 +3,7 @@
 using namespace std;
 int main()
 {
-    vector<int> ivec = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    vector<int>::size_type cnt = ivec.size();
-    //将把从size到1的值赋给ivec的元素
-    for (vector<int>::size_type ix = 0; ix != ivec.size(); ++ix, --cnt)
-        cout << (ivec[ix] = cnt) << " ";
-    cout << endl;
+    //重复读入数据直至到达文件末尾或某次输入的值等于sought
+    while (cin >> s && s != sought)
+        ; //空语句
 }
